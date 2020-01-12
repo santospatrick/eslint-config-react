e53edea59dd361513dc21dbbe7567bd8e2395851
Patrick Santos <patrickspteixeira@gmail.com>
2020-01-12 15:12:29 -0300

    adiciona prettier

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = {
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended'
+        'plugin:react/recommended',
+        'plugin:prettier/recommended'
     ],
     env: {
         browser: true,
